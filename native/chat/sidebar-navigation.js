4341489f62200465bb774e97d8ac50c858b9b079
Tomasz Palys <tomasz.palys@swmansion.com>
2021-06-10 13:37:07 +0200

    [native] Merge sidebar navigation functions
    Summary: The functions are now the same so it makes sense to merge them together.
    
    Test Plan:
    
    Reviewers: ashoat

--- native/chat/sidebar-navigation.js
+++ native/chat/sidebar-navigation.js
@@ -8,7 +8,7 @@ import { MessageListRouteName } from '../navigation/route-names';
 import type { TooltipRoute } from '../navigation/tooltip.react';
 import { getSidebarThreadInfo } from './utils';
 
-function onPressGoToSidebar(
+function navigateToSidebar(
   route:
     | TooltipRoute<'RobotextMessageTooltipModal'>
     | TooltipRoute<'TextMessageTooltipModal'>
@@ -39,35 +39,4 @@ function onPressGoToSidebar(
   });
 }
 
-function onPressCreateSidebar(
-  route:
-    | TooltipRoute<'RobotextMessageTooltipModal'>
-    | TooltipRoute<'TextMessageTooltipModal'>
-    | TooltipRoute<'MultimediaTooltipModal'>,
-  dispatchFunctions: DispatchFunctions,
-  bindServerCall: <F>(serverCall: ActionFunc<F>) => F,
-  inputState: ?InputState,
-  navigation:
-    | AppNavigationProp<'RobotextMessageTooltipModal'>
-    | AppNavigationProp<'TextMessageTooltipModal'>
-    | AppNavigationProp<'MultimediaTooltipModal'>,
-  viewerID: ?string,
-) {
-  let threadInfo;
-  // Necessary for Flow...
-  if (route.name === 'RobotextMessageTooltipModal') {
-    threadInfo = getSidebarThreadInfo(route.params.item, viewerID);
-  } else {
-    threadInfo = getSidebarThreadInfo(route.params.item, viewerID);
-  }
-
-  navigation.navigate({
-    name: MessageListRouteName,
-    params: {
-      threadInfo,
-    },
-    key: `${MessageListRouteName}${threadInfo.id}`,
-  });
-}
-
-export { onPressGoToSidebar, onPressCreateSidebar };
+export { navigateToSidebar };
